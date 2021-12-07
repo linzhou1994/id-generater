8fec18eca2fe78526ba38ca528e72d8ba762115a
linzhou <linzhou@duiba.com.cn>
2021-12-07 16:44:42 +0800

    init

---
+++ src/main/java/com/id/generater/IdGeneraterApplication.java
@@ -0,0 +1,13 @@
+package com.id.generater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdGeneraterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdGeneraterApplication.class, args);
+    }
+
+}
